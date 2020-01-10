d46bb6efc41ad3d5c3172d47118945a9a831cabe
jason <jason_doan94@yahoo.com>
2020-01-10 03:41:27 -0500

    state and props burger.js

--- burgerapp/src/components/Burger/Burger.js
+++ burgerapp/src/components/Burger/Burger.js
@@ -4,11 +4,21 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    //map out the ingredients
+    .map(igKey => {
+      //transform the string value into an array with as many elements for given ingredients
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+        //return BurgerIngredient because need to return an array of JSX
+        //igkey ie: salad, cheese, etc, i = 1,2,3,etc
+      });
+      //access given "key" because still in igkey function of map method which runs in an array which contains all keys
+    });
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
