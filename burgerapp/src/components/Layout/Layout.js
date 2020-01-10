bdf795128c9e6fb9cfe8fa7be123f65af609ee3a
jason <jason_doan94@yahoo.com>
2020-01-10 03:11:29 -0500

    .module.css

--- burgerapp/src/components/Layout/Layout.js
+++ burgerapp/src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Aux from "../../hoc/Aux";
-import classes from "../Layout/Layout.css";
+import classes from "../Layout/Layout.module.css";
 
 const layout = props => (
   <Aux>
